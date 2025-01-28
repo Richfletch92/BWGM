0f3fdb12eccef9a2b9cddc856f64d4b29c90b406
richfletch92 <richfletch92@gmail.com>
2025-01-28 09:39:44 +0000

    add if statement to debug

--- bwgm/settings.py
+++ bwgm/settings.py
@@ -28,7 +28,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
+if DEBUG is None:
+    DEBUG = False
+else:
+    DEBUG = DEBUG.lower() in ['true', '1', 't', 'yes', 'y']
 
+
+print(DEBUG)
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 # Application definition
