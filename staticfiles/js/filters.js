4239ef2153b54769f26907d9cbe10d0ba98e0040
richfletch92 <richfletch92@gmail.com>
2025-02-06 11:57:46 +0000

    collect static ran

--- staticfiles/js/filters.js
+++ staticfiles/js/filters.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const toggleFilterButton = document.getElementById("toggleFilterButton");
     const filterFormContainer = document.getElementById("filterFormContainer");
 
+    // Check if filters are applied
+    const urlParams = new URLSearchParams(window.location.search);
+    const filtersApplied = Array.from(urlParams.keys()).length > 0;
+
+    if (filtersApplied) {
+        filterFormContainer.style.display = "block";
+        toggleFilterButton.textContent = "Hide Filters";
+    }
+
     toggleFilterButton.addEventListener("click", function() {
         if (filterFormContainer.style.display === "none") {
             filterFormContainer.style.display = "block";
@@ -11,5 +20,4 @@ document.addEventListener("DOMContentLoaded", function() {
             toggleFilterButton.textContent = "Show Filters";
         }
     });
-}
-if
+});
