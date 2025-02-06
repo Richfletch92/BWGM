4f0e00591c73f2c780d3220daef2c7a89182f719
richfletch92 <richfletch92@gmail.com>
2025-02-06 11:17:10 +0000

    add cloud database

---
+++ staticfiles/js/filters.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleFilterButton = document.getElementById("toggleFilterButton");
+    const filterFormContainer = document.getElementById("filterFormContainer");
+
+    toggleFilterButton.addEventListener("click", function() {
+        if (filterFormContainer.style.display === "none") {
+            filterFormContainer.style.display = "block";
+            toggleFilterButton.textContent = "Hide Filters";
+        } else {
+            filterFormContainer.style.display = "none";
+            toggleFilterButton.textContent = "Show Filters";
+        }
+    });
+}
+if
